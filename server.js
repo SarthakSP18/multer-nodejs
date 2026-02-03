0c51706b3964f34d2921f9fa3832e8353f9a227f
SarthakSP18 <sarthakpomane123@gmail.com>
2026-02-03 22:49:31 +0530

    multer

---
+++ server.js
@@ -0,0 +1,39 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const app = express();
+
+const port = 5000;
+
+const storage = multer.diskStorage({
+    destination : function (req,file,cb){
+        return cb(null,"./uploads")
+    },
+    filename:function(req,file,cb){
+        return cb(null,`${Date.now()}-${file.originalname}`)
+    }
+    
+})
+
+const upload = multer({storage})
+
+app.set("view engine","ejs");
+
+app.set("views",path.resolve("./views"));
+
+// app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.get("/", (req,res)=>{
+    return res.render("homepage")
+})
+app.post("/upload",upload.single("profileimage"),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect("/");
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on http://localhost:${port}`);
+})
